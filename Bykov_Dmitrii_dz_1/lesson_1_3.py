d7a24bb6bbd9dddbb69d2941673f2d8aecbbab61
Pro200G <78156285+Pro200G@users.noreply.github.com>
2021-02-18 16:17:38 +0300

    Delete Bykov_Dmitrii_dz_1 directory
    This is a past lesson

--- Bykov_Dmitrii_dz_1/lesson_1_3.py
+++
@@ -1,8 +0,0 @@
-percent = 0
-for percent in range(1, 21):
-    if percent == 1:
-        print(percent, 'процент')
-    elif percent >= 2 and percent <= 4:
-        print(percent, 'процента')
-    elif percent >= 5 and percent <= 20:
-        print(percent, 'процентов')
